88502908b5de77a63972410196782669b9fdc641
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-07-03 22:02:00 +0530

    fixed error

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
-import { Box, Image, Text } from '@chakra-ui/react';
+import { Box, Image } from '@chakra-ui/react';
 import React from 'react';
-import btcSrc from '../assets/btc (1).png'
+import btcSrc from './btc (1).png'
 const Home = () => {
   return (
     <Box bgColor={'blackAlpha.900'} w={'full'} h={'85vh'}>
